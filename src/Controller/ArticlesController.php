dfd73a0921815f6897b94190ead5ed7acc6c628a
eorin <povorovegor@yandex.ru>
2023-03-26 19:33:59 +0300

    EORIN

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ArticlesRepository;
+
+class ArticlesController extends AbstractController
+{
+
+    public function __construct(private ArticlesRepository $articlesRepository)
+    {
+
+    }
+
+    #[Route('/articles', name: 'app_articles')]
+    public function index(): Response
+    {
+        return $this->render('articles/index.html.twig', [
+            'controller_name' => 'ArticlesController',
+            'articles' => ['91919'],
+        ]);
+    }
+}
---
+++ src/Entity/Articles.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticlesRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticlesRepository::class)]
+class Articles
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $Title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $Text = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): self
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->Text;
+    }
+
+    public function setText(string $Text): self
+    {
+        $this->Text = $Text;
+
+        return $this;
+    }
+}
